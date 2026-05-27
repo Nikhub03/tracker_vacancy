620cd07ead8d776a0ddad7664c3cda38cdab7010
Nikhub03 <nkozinets03@mail.ru>
2026-05-27 13:33:03 +0300

    Update database.py

--- app/database.py
+++ app/database.py
@@ -1,29 +1,21 @@
-# app/database.py
 from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 
-# Для MVP мы будем использовать простую и удобную SQLite
-# Файл базы данных 'hh_tracker.db' автоматически создастся в корневой папке проекта
+# Обращение к БД или ее создание если ее не существует
 SQLALCHEMY_DATABASE_URL = "sqlite:///./hh_tracker.db"
 
-# `engine` - это "двигатель", который будет физически подключаться к нашей базе данных
-# `connect_args={"check_same_thread": False}` - специальный параметр, необходимый для SQLite,
-# который разрешает доступ к базе данных из разных потоков. В реальном проекте с PostgreSQL он не нужен.
 engine = create_engine(
     SQLALCHEMY_DATABASE_URL, connect_args={"check_same_thread": False}
 )
 
-# `SessionLocal` - это "фабрика" по созданию сессий.
-# Сессия - это конкретное подключение к базе данных, через которое мы будем выполнять операции.
+# Создание сессии
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 
-# `Base` - это фундамент для всех наших моделей (таблиц).
-# Мы будем наследовать наши классы таблиц от этого класса.
 Base = declarative_base()
 
-# Эта функция нужна для того, чтобы FastAPI мог автоматически управлять жизненным циклом сессии.
-# Каждый новый запрос к серверу будет создавать новую сессию, а после обработки запроса - закрывать её.
+# Автоматическое управление сессиями FastAPI,
+# где каждый новый запрос к серверу создает новую сессию, потом после обработки запроса ее закрывает
 def get_db():
     db = SessionLocal()
     try:
